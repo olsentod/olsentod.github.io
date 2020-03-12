3854a1cbb5739029d71065620d327da394fb5f5a
olsentod <olsentod@gmail.com>
2020-03-11 23:13:35 -0600

    Updated Sass. And added new content

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('mobile-menu').addEventListener('click', function(){
-        document.querySelector('nav > ul.navbar').classList.toggle('open');
-    })
+        document.querySelector('nav').classList.toggle('open');
+    });
+    document.getElementById('mobile-menu-close').addEventListener('click', function(){
+        document.querySelector('nav').classList.toggle('open');
+    });
 });
